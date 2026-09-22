b3dbc1a5343ad0e9745aef57d5c7fafa01460b1c
priyanjal <pmourya3116@gmail.com>
2026-09-22 22:18:20 +0530

    test Script

--- playwright.config.js
+++ playwright.config.js
@@ -44,10 +44,7 @@ export default defineConfig({
     // Main browser tests
     {
       name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        storageState: 'playwright/.auth/user.json',
-      },
+      
       dependencies: ['setup'],
     },
 
